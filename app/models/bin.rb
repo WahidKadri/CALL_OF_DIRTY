e9eeb428698935e6048bb74d6747d5bb91e77f3a
Wahid Kadri <wahidkadri.motiondesigner@gmail.com>
2018-05-28 16:42:08 +0200

    validations & association ok

--- app/models/bin.rb
+++ app/models/bin.rb
@@ -1,2 +1,7 @@
 class Bin < ApplicationRecord
+  has_many :packagings
+  has_many :products, through: :packagings
+
+  validates :color, presence: true
+  validates :color, inclusion: { in: %w(jaune blanc vert) }
 end
