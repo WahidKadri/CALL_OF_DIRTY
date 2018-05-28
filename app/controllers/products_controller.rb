6b69cbc2334d55c0104f8e3293b10fd2c7680067
Wahid Kadri <wahidkadri.motiondesigner@gmail.com>
2018-05-28 19:02:32 +0200

    parsing open food fact API

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,21 @@
+require 'open-uri'
+require 'json'
+
 class ProductsController < ApplicationController
 
   def show
-    @product = Product.find(params[:id])
+    # @product = Product.find(params[:id])
+    @bar_code = "8715700423913"
+    url = "https://fr.openfoodfacts.org/api/v0/produit/'#{@bar_code}'.json"
+    product_serialized = open(url).read
+    parsed_product = JSON.parse(product_serialized)
+    @name = parsed_product["product"]["product_name_fr"]
+    @brand = parsed_product["product"]["brands"]
+    @photo = parsed_product["product"]["image_front_small_url"]
+    @description = parsed_product["product"]["categories"]
+
+    Product.create(name: @brand, brand: @brand, photo: @photo, description: @description)
+    end
   end
 
   def new
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,7 @@ class Product < ApplicationRecord
   has_many :bins, through: :packagings
   has_many :scans
 
-  validates :brand, :description, :bar_code, presence: true
+  validates :brand, :name, :description, :bar_code, presence: true
   validates :bar_code, uniqueness: true
 end
+
