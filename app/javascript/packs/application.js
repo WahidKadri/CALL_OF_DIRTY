3da8f5dcb1f6c95fa94133074f327bdfe076d483
Alexandre Van Besien <alexandre.vanbesien@gmail.com>
2018-05-29 10:25:02 +0200

    activation of the camera

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,1 +1,20 @@
 import "bootstrap";
+import Quagga from 'quagga'; // ES6
+
+Quagga.init({
+    inputStream : {
+      name : "Live",
+      type : "LiveStream",
+      target: document.querySelector('#barcode-scanner')    // Or '#yourElement' (optional)
+    },
+    decoder : {
+      readers : ["code_128_reader"]
+    }
+  }, function(err) {
+      if (err) {
+          console.log(err);
+          return
+      }
+      console.log("Initialization finished. Ready to start");
+      Quagga.start();
+  });
