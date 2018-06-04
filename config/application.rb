00e600533e0cde6a65915ccc42f28f3f3a946dc0
Wahid Kadri <wahidkadri.motiondesigner@gmail.com>
2018-06-04 10:35:47 +0200

    set local hour

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module CALLOfDirty
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Paris'
+    config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
