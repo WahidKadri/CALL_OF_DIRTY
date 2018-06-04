275c4847b456710475ed44e6515ed79dcfcb5a71
Wahid Kadri <wahidkadri.motiondesigner@gmail.com>
2018-06-04 17:10:57 +0200

    modal & trash icons

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,6 @@
 import "bootstrap";
-import Quagga from 'quagga'; // ES6
+// import Quagga from 'quagga'; // ES6
+import { addBorderToButton } from "../components/add_border_to_button.js";
 
 $(document).ready(function(){
   $(".category-choice").click(function(){
@@ -7,30 +8,32 @@ $(document).ready(function(){
   });
 });
 
-Quagga.init({
-  inputStream : {
-    name : "Live",
-    type : "LiveStream",
-    target: document.querySelector('#barcode-scanner')    // Or '#yourElement' (optional)
-  },
-  decoder : {
-    readers : ['ean_reader']
-  }
-}, function(err) {
-  if (err) {
-      console.log(err);
-      return
-  }
-  console.log("Initialization finished. Ready to start");
-  Quagga.start();
-});
+// Quagga.init({
+//   inputStream : {
+//     name : "Live",
+//     type : "LiveStream",
+//     target: document.querySelector('#barcode-scanner')    // Or '#yourElement' (optional)
+//   },
+//   decoder : {
+//     readers : ['ean_reader']
+//   }
+// }, function(err) {
+//   if (err) {
+//       console.log(err);
+//       return
+//   }
+//   console.log("Initialization finished. Ready to start");
+//   Quagga.start();
+// });
 
-Quagga.onDetected(function(result) {
-  const input = document.getElementById('product_bar_code');
-  input.value = result.codeResult.code
-  const form = document.getElementById('new_product')
-  form.submit();
-  Quagga.stop()
-});
+// Quagga.onDetected(function(result) {
+//   const input = document.getElementById('product_bar_code');
+//   input.value = result.codeResult.code
+//   const form = document.getElementById('new_product')
+//   form.submit();
+//   Quagga.stop()
+// });
+
+addBorderToButton();
 
 
