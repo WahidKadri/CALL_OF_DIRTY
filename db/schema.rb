5f2fe1f3f6e4cae4edb99f97ab26a67b595800d2
Wahid Kadri <wahidkadri.motiondesigner@gmail.com>
2018-06-05 17:29:42 +0200

    upload photo ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_141800) do
+ActiveRecord::Schema.define(version: 2018_06_05_151733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,7 @@ ActiveRecord::Schema.define(version: 2018_06_05_141800) do
     t.datetime "token_expiry"
     t.integer "score", default: 0, null: false
     t.string "pseudo"
+    t.string "photo"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
